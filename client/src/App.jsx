e3c101a98c1fd59950cf717117cc23e47dca2dc7
neo2wicked <capricorn@Live.com>
2023-10-24 16:14:55 -0500

    Update header and make profile page private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,9 @@ export default function App() {
       <Route path="/about" element={<About />} />
       <Route path="/sign-in" element={<SignIn />} />
       <Route path="/sign-up" element={<SignUp />} />
+      <Route element={<PrivateRoute />}>
       <Route path="/profile" element={<Profile />} />
+      </Route>
 
     </Routes>
     
